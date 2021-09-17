6dda59482628f8689ef39a1e964e27ac5a4729de
Prashanth <prashanthprabhu1998@gmail.com>
2021-09-17 22:05:45 +0530

    Add files via upload

---
+++ RSA/primeSieve.py
@@ -0,0 +1,23 @@
+import math
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def primeSieve(sieveSize):
+    sieve = [True] * sieveSize
+    sieve[0] = False  # zero and one are not prime numbers
+    sieve[1] = False
+    for i in range(2, int(math.sqrt(sieveSize)) + 1):
+        pointer = i * 2
+        while pointer < sieveSize:
+            sieve[pointer] = False
+            pointer += i
+    primes = []
+    for i in range(sieveSize):
+        if sieve[i] == True:
+            primes.append(i)
+    return primes
