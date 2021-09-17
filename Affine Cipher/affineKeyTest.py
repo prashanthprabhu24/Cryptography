0493cc70580a5daf5358fd8d98d2a5a3446961d5
Prashanth <prashanthprabhu1998@gmail.com>
2021-09-17 20:46:42 +0530

    Add files via upload

---
+++ Affine Cipher/affineKeyTest.py
@@ -0,0 +1,6 @@
+import affineCipher, cryptomath
+message = 'Make things as simple as possible, but not simpler.'
+for keyA in range(2, 100):
+    key = keyA * len(affineCipher.SYMBOLS) + 1
+    if cryptomath.gcd(keyA, len(affineCipher.SYMBOLS)) == 1:
+        print(keyA, affineCipher.encryptMessage(key, message))
