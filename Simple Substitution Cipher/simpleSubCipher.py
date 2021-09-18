51f828a3eca942a3623bb40f1e8c2a11898f9bf7
Prashanth <prashanthprabhu1998@gmail.com>
2021-09-18 18:25:55 +0530

    Update simpleSubCipher.py

--- Simple Substitution Cipher/simpleSubCipher.py
+++ Simple Substitution Cipher/simpleSubCipher.py
@@ -1,20 +1,25 @@
 import pyperclip, sys, random
 LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 def main():
-    myMessage = """If a man is offered a fact which goes against his instincts, he will scrutinize it closely, 
-    and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered 
-    something which affords a reason for acting in accordance to his instincts, he will accept it even on the 
-    slightest evidence. The origin of myths is explained in this way. -Bertrand Russell """
-    myMessage = """Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, 
-    lxo txmarr jia aqsoaxwa sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr pyyacao 
-    rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia 
-    rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ctrramm"""
-    myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
-    myMode = 'decrypt'
+    # myMessage = """If a man is offered a fact which goes against his instincts, he will scrutinize it closely, 
+    # and unless the evidence is overwhelming, he will refuse to believe it. If, on the other hand, he is offered 
+    # something which affords a reason for acting in accordance to his instincts, he will accept it even on the 
+    # slightest evidence. The origin of myths is explained in this way. -Bertrand Russell """
+    # myMessage = """Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, 
+    # lxo txmarr jia aqsoaxwa sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr pyyacao 
+    # rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia 
+    # rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ctrramm"""
+    # myKey = 'LFWOAYUISVKMNXPBDCRJTQEGHZ'
+    print('Enter the Plain/Cipher Text')
+    myMessage = input('> ')
+    print('Enter the Key')
+    myKey = input('> ')
+    print('Enter the Mode of operation, (E)ncryption or (D)ecryption')
+    myMode = input('> ')[0].lower()
     checkValidKey(myKey)
-    if myMode == 'encrypt':
+    if myMode == 'e':
         translated = encryptMessage(myKey, myMessage)
-    elif myMode == 'decrypt':
+    elif myMode == 'd':
         translated = decryptMessage(myKey, myMessage)
     print('Using key %s' % (myKey))
     print('The %sed message is:' % (myMode))
